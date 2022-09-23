c1735a44bfc8ee0c3394d11c8f1fc39fd942a0d8
FiezDev <79656850+FiezDev@users.noreply.github.com>
2022-09-24 01:37:52 +0700

    tailwind test

--- admin/urls.py
+++ admin/urls.py
@@ -22,15 +22,18 @@ from django.views.generic import TemplateView
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('index/', TemplateView.as_view(template_name='index.html')),
+
     path('', TemplateView.as_view(template_name='registrations/login.html'),
          name='main'
          ),
-    path('phoenix/', TemplateView.as_view(template_name='phoenix.base.html'),
-         name='phoenixv2'),
     path('twostep/', TemplateView.as_view(template_name='components/twostep.html'),
          name='twostep'
          ),
+    
+    path('phoenix/', TemplateView.as_view(template_name='phoenix.base.html'),
+         name='phoenixv2'),
+    
+
     path('phoenix/index/', TemplateView.as_view(template_name='components/index.html'),
          name='index'
          ),
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,44 @@ module.exports = {
     './templates/**/*.html',
 ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+
+      primary: '#1c84ee',
+      secondary:' #74788d',
+
+      success: '#34c38f',
+      info: '#16daf1',
+      warning: '#ffcc5a',
+      danger: '#ef6767',
+      purple:  '#6f42c1',
+
+      light: '#f6f6f6',
+      dark: '#2b3940',
+      
+      white: '#fff',
+      black: '#000',
+    },
     extend: {},
   },
   plugins: [],
+  extend: {
+    screens: {
+      'xs': '400px',
+      'ssm': '576px',
+
+      // 'bootstrap-md': '768px',
+      // 'bootstrap-lg': '992px',
+      // 'bootstrap-xl': '1200px',
+      // 'bootstrap-xxl': '1400px'
+
+      // sm	640px	
+      // md	768px
+      // lg	1024px
+      // xl	1280px
+      // 2xl	1536px
+    }
+  }
 }
+
+
